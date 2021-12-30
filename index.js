45b3fae176b9641357fe44f80ad30bfb5aad0dd8
Aleksei <abolshoff@yandex.ru>
2021-12-30 14:46:49 +0900

    add project

---
+++ index.js
@@ -0,0 +1,443 @@
+'use strict'
+const overall = document.getElementById('overallChart');
+const bread = document.getElementById('bread');
+const potato = document.getElementById('potato');
+const vegetables = document.getElementById('vegetables');
+const fruits = document.getElementById('fruits');
+const sugar = document.getElementById('sugar');
+const meat = document.getElementById('meat');
+const fish = document.getElementById('fish');
+const milk = document.getElementById('milk');
+const eggs = document.getElementById('eggs');
+const oil = document.getElementById('oil');
+const tea = document.getElementById('tea');
+const overallChart = new Chart(overall, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [12, 25, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const breadChart = new Chart(bread, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const potatoChart = new Chart(potato, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const vegetablesChart = new Chart(vegetables, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const fruitsChart = new Chart(fruits, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const sugarChart = new Chart(sugar, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const meatChart = new Chart(meat, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const fishChart = new Chart(fish, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const milkChart = new Chart(milk, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const eggsChart = new Chart(eggs, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const oilChart = new Chart(oil, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+const teaChart = new Chart(tea, {
+    type: 'line', //bar
+    data: {
+        labels: [2016, 2017, 2018, 2019, 2020, 2021],
+        datasets: [{
+            label: 'Уровень инфляции, %',
+            data: [15, 20, 10, 7, 13, 9],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
+
+//basket-list
+const basketHeader = document.querySelector('.basket-header')
+const basketList = document.querySelector('.basket-list')
+
+basketHeader.addEventListener('click', ()=>{
+    basketList.classList.toggle('hidden')
+})
+
+//---------------- Chart toggle---------------------//
+
+const listItem = document.querySelectorAll('.basket-item')
+const chartContainer = document.querySelectorAll('.chart-container')
+
+$(document).ready(function(){
+        $(".basket-item").click(function(){
+            $(".chart-container").eq($(".basket-item").index(this)).addClass("active").siblings().removeClass("active");
+            $(".chart-container-overall").removeClass("active");
+        });
+    }
+
+);
